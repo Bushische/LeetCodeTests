1e474f04173c38eec2782919aaa93c698c7b7597
Bushische <bushuev.a.a@list.ru>
2018-03-04 22:23:19 +0300

    Solution for: 40. Combination Sum II

---
+++ Medium/Problem_0040.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace LeetCodeTests
+{
+    public abstract class Problem_0040
+    {
+        /*
+         * 40. Combination Sum II
+Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+Each number in C may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+
+For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8, 
+A solution set is: 
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+		*/
+        public static void Test()
+        {
+            Solution sol = new Solution();
+
+            /*
+			var input = new int[] { 2, 7, 11, 15 };
+			Console.WriteLine($"Input array: {string.Join(", ", input)}");
+			*/
+            int[] inList = new int[] { 10, 1, 2, 7, 6, 1, 5 };
+            int target = 8;
+            var outList = sol.CombinationSum2(inList, target);
+            Console.WriteLine($"Input: {Utils.PrintArray(inList)}, target : {target}, output: {Utils.PrintArray(outList)}");
+
+            target = 40;
+            outList = sol.CombinationSum2(inList, target);
+            Console.WriteLine($"Input: {Utils.PrintArray(inList)}, target : {target}, output: {Utils.PrintArray(outList)}");
+
+            inList = new int[0];
+            target = 40;
+            outList = sol.CombinationSum2(inList, target);
+            Console.WriteLine($"Input: {Utils.PrintArray(inList)}, target : {target}, output: {Utils.PrintArray(outList)}");
+
+        }
+
+        public class Solution
+        {
+            private int Target;
+            private int Length;
+            IList<IList<int>> ResList;
+            int[] Elements;
+
+            public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+            {
+                Target = target;
+                Length = candidates.Length;
+                ResList = new List<IList<int>>();
+                Array.Sort(candidates);
+                Elements = candidates;
+
+                var tList = new List<int>();
+                ProcessNextVariant(0, tList, 0);
+
+                return ResList;
+            }
+
+            private void ProcessNextVariant(int left, List<int> testCand, int sum)
+            {
+                int prevElement = -1;
+                while (left < Length)
+                {
+                    int elem = Elements[left];
+
+                    if (prevElement != elem)
+                    {
+                        if (sum + elem == Target)
+                        {
+                            testCand.Add(elem);
+                            FixVariant(testCand);
+                            testCand.RemoveAt(testCand.Count - 1);
+                        }
+                        else if (sum + elem < Target)
+                        {
+                            testCand.Add(elem);
+                            ProcessNextVariant(left + 1, testCand, sum + elem);
+                            testCand.RemoveAt(testCand.Count - 1);
+                        }
+                        else if (sum + elem > Target)
+                        {
+                            // should stop process
+                            return;
+                        }
+                    }
+                    left++;
+                    prevElement = elem;
+                }
+            }
+
+            private void FixVariant(List<int> inList)
+            {
+                var newVar = new List<int>(inList);
+                ResList.Add(newVar);
+            }
+        }
+    }//public abstract class Problem_
+}
--- Program.cs
+++ Program.cs
@@ -8,76 +8,79 @@ namespace LeetCodeTests
 		{
 			Console.WriteLine("LeetCode tests in c#");
 
-			#region first 50
-			//Problem_0001.Test();
-			//Problem_0002.Test();
-			//Problem_0003.Test();
-			//Problem_0007.Test();
-			// ?? Problem_0004.Test();
-			//Problem_0009.Test();
-			//Problem_0008.Test();
-			//Problem_0006.Test();
-			//Problem_0005.Test();
-			//Problem_0013.Test();
-			//Problem_0014.Test();
-			//Problem_0011.Test();
-			//Problem_0012.Test();
-			//Problem_0015.Test();
-			//Problem_0016.Test();
-			//Problem_0017.Test();
-			//Problem_0020.Test();
-			//Problem_0019.Test();
-			//Problem_0021.Test();
-			//Problem_0018.Test();
-			//Problem_0022.Test();
-			//Problem_0010.Test();
-			//Problem_0024.Test();
-			//Problem_0029.Test();
-			//Problem_0027.Test();
-			//Problem_0031.Test();
-			//Problem_0033.Test();
-			//Problem_0036.Test();
-			//Problem_0037.Test();
-			//Problem_0039.Test();
-			//Problem_0043.Test();
-			//Problem_0049.Test();
-			//Problem_0046.Test();
-			//Problem_0048.Test();
-			//Problem_0042.Test();
-			//Problem_0050.Test();
-			#endregion
+            #region first 50
+            //Problem_0001.Test();
+            //Problem_0002.Test();
+            //Problem_0003.Test();
+            //Problem_0007.Test();
+            // ?? Problem_0004.Test();
+            //Problem_0009.Test();
+            //Problem_0008.Test();
+            //Problem_0006.Test();
+            //Problem_0005.Test();
+            //Problem_0013.Test();
+            //Problem_0014.Test();
+            //Problem_0011.Test();
+            //Problem_0012.Test();
+            //Problem_0015.Test();
+            //Problem_0016.Test();
+            //Problem_0017.Test();
+            //Problem_0020.Test();
+            //Problem_0019.Test();
+            //Problem_0021.Test();
+            //Problem_0018.Test();
+            //Problem_0022.Test();
+            //Problem_0010.Test();
+            //Problem_0024.Test();
+            //Problem_0029.Test();
+            //Problem_0027.Test();
+            //Problem_0031.Test();
+            //Problem_0033.Test();
+            //Problem_0036.Test();
+            //Problem_0037.Test();
+            //Problem_0039.Test();
+            //Problem_0043.Test();
+            //Problem_0049.Test();
+            //Problem_0046.Test();
+            //Problem_0048.Test();
+            //Problem_0042.Test();
+            //Problem_0050.Test();
+            Problem_0040.Test();
+            #endregion
 
-			//Problem_0051.Test();		//hard
-			//Problem_0054.Test();		//medium
-			//Problem_0056.Test();		//medium
-			//Problem_0061.Test();		//medium
-			//Problem_0062.Test();		//medium
-			//Problem_0063.Test();		//medium
-			//Problem_0066.Test();		//easy
-			//Problem_0065.Test();		//hard
-			//Problem_0071.Test();		//medium
-			//Problem_0072.Test();		//hard
-			//Problem_0060.Test();		//medium
-			//Problem_0069.Test();		//easy
-			//Problem_0074.Test();		//medium
-			//Problem_0075.Test();		//medium
-			//Problem_0073.Test();		//medium
-			//Problem_0077.Test();		//medium
-			//Problem_0078.Test();		//medium
-			//Problem_0082.Test();		//medium
-			//Problem_0086.Test();		//medium
-			//Problem_0067.Test();		//easy
-			//Problem_0058.Test();		//easy
-			//Problem_0079.Test();		//medium
-			//Problem_0087.Test();		//hard
-			//Problem_0088.Test();		//easy
-			//Problem_0089.Test();		//medium
-			//Problem_0091.Test();		//medium
-			Problem_0092.Test();		//medium
+            #region Second 50
+            //Problem_0051.Test();		//hard
+            //Problem_0054.Test();		//medium
+            //Problem_0056.Test();		//medium
+            //Problem_0061.Test();		//medium
+            //Problem_0062.Test();		//medium
+            //Problem_0063.Test();		//medium
+            //Problem_0066.Test();		//easy
+            //Problem_0065.Test();		//hard
+            //Problem_0071.Test();		//medium
+            //Problem_0072.Test();		//hard
+            //Problem_0060.Test();		//medium
+            //Problem_0069.Test();		//easy
+            //Problem_0074.Test();		//medium
+            //Problem_0075.Test();		//medium
+            //Problem_0073.Test();		//medium
+            //Problem_0077.Test();		//medium
+            //Problem_0078.Test();		//medium
+            //Problem_0082.Test();		//medium
+            //Problem_0086.Test();		//medium
+            //Problem_0067.Test();		//easy
+            //Problem_0058.Test();		//easy
+            //Problem_0079.Test();		//medium
+            //Problem_0087.Test();		//hard
+            //Problem_0088.Test();		//easy
+            //Problem_0089.Test();		//medium
+            //Problem_0091.Test();		//medium
+            //Problem_0092.Test();        //medium
+            #endregion
 
-			//TODO: Problem_0084; 		//hard
+            //TODO: Problem_0084; 		//hard
 
-			Console.WriteLine("that's all");
+            Console.WriteLine("that's all");
 			Console.ReadKey();
 		}
 	}
