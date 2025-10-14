ba32de8d72f656894d6a8c60da681e9521da1fbc
Alex Bu <sanek.bushuev@gmail.com>
2025-10-14 22:52:19 +0700

    Update project to .net core 9, add test project

---
+++ LeetCodeTests.UnitTests/Problem_0087Tests.cs
@@ -0,0 +1,38 @@
+using System.Runtime.Intrinsics.Arm;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem_0087Tests
+    {
+        [TestCase("rgeat", "great", true)]      //true
+        [TestCase("rgtae", "great", true)]
+        [TestCase("rtgae", "great", false)]
+        [TestCase("dcba", "abcd", true)]
+        [TestCase("dabc", "abcd", true)]
+        [TestCase("abc", "bac", true)]
+        [TestCase("great", "great", true)]
+        [TestCase("a", "ab", false)]
+        [TestCase("", "", true)]
+        [TestCase("a", "a", true)]
+        [TestCase("a", "b", false)]
+        public void IsScramble_VariousCases(string s1, string s2, bool expected)
+        {
+            var sol = new Problem_0087.Solution();
+            bool actual = sol.IsScramble(s1, s2);
+            Assert.AreEqual(expected, actual, $"IsScramble(\"{s1}\", \"{s2}\") should be {expected}");
+        }
+
+        [Test]
+        public void IsScramble_VariousCases2()
+        {
+            string s1 = "rgeat";
+            string s2 = "great";
+            bool expected = true;
+            var sol = new Problem_0087.Solution();
+            bool actual = sol.IsScramble(s1, s2);
+            Assert.AreEqual(expected, actual, $"IsScramble(\"{s1}\", \"{s2}\") should be {expected}");
+        }
+    }
+}
